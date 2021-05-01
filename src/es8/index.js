ac9a5ce0f6e3afcccf242ea99149c26d97353d4c
Brian <jacklevybrsg@gmail.com>
2021-05-01 11:48:36 -0500

    Async Await

--- src/es8/index.js
+++ src/es8/index.js
@@ -18,3 +18,30 @@ const string = 'hello'
 console.log(string.padStart(7,'hi'))
 console.log(string.padEnd(12, ' -----'))
 console.log('food'.padEnd(12, '  -----'))
+
+
+////////
+
+const helloWorld = () => {
+    return new Promise((resolve, reject) => {
+        (false) 
+            ? setTimeout(() => resolve('Hello World'), 3000)
+            : reject(new Error('Test Error'))
+    })
+}
+
+const helloAsync = async () => {
+    const hello = await helloWorld()
+    console.log(hello)
+}
+
+helloAsync()
+
+const anotherFunction = async () => {
+    try {
+        const hello = await helloWorld()
+        console.log(hello)
+    } catch (error) {
+        console.log(error)
+    }
+}
