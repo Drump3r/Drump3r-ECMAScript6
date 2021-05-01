355e787ee09a65183bdec4d5d12ea00699b1070c
Brian <jacklevybrsg@gmail.com>
2021-05-01 11:31:39 -0500

    ¿Qué se implementó en ES7?

---
+++ src/es7/index.js
@@ -0,0 +1,17 @@
+/*.includes*/
+
+let numbers = [1,2,7,6,8,5,4]
+
+if (numbers.includes(7)) {
+    console.log('Sí se encuentra el valor 7')
+} else {
+    console.log('No se encuentra')
+}
+
+/** Potencias */
+let base = 4
+let exponnet = 3
+let result = base ** exponnet
+
+console.log(result)
+
